0ccc29e170d9e29bc5c59f5498a009887c2e207b
gr5 <you@example.com>
2021-10-02 19:00:58 +0530

    Added mailer coinfiguration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,19 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://devise-mail.herokuapp.com" }
 
-  config.action_mailer.default_url_options = { host: "www.example.com" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    user_name:      'ami.logicalstreet@gmail.com',
+    password:       'Ami@1111',
+    domain:         'MAIL_HOST',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
 end
